211eaf13ae3cba6b770dd5c921ca179eec3aa4e3
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-10-20 23:38:48 -0700

    Extract user's PS1 and interpret most of it

--- Sources/REPL/main.swift
+++ Sources/REPL/main.swift
@@ -1,7 +1,13 @@
 import Foundation
 
 let command = CommandLine.arguments.dropFirst().joined(separator: " ")
-let cmdPrompt = "\u{001B}[38;5;9m[\(NSUserName())]\u{001B}[1;34m(\(FileManager.default.currentDirectoryPath.replacingOccurrences(of: "/Users/\(NSUserName())", with: "~")))\u{001B}[m\u{001B}[1;32m$ \(command)\u{001B}[00m "
+//func posix_exec(_ args: [String]) {
+//    var pid: pid_t = 0
+//    let args = ["/usr/bin/env", "bash", "-i", "-c", #"echo "$PS1""#]
+//    let c_args = args.map { $0.withCString(strdup)! }
+//    posix_spawn(&pid, c_args[0], nil, nil, c_args + [nil], environ)
+//    waitpid(pid, nil, 0)
+//}
 
 var task: Process = Process()
 func exec(_ command: String) -> String {
@@ -19,6 +25,60 @@ func exec(_ command: String) -> String {
     return output
 }
 
+// MARK: - Extract PS1
+//   (via `bash -i -c 'echo "$PS1"'`)
+var action: posix_spawn_file_actions_t? = nil
+posix_spawn_file_actions_init(&action);
+defer { posix_spawn_file_actions_destroy(&action) }
+let m: mode_t = S_IRWXU | S_IRWXG | S_IRWXO
+posix_spawn_file_actions_addopen(&action, 1, "/tmp/ps1", (O_WRONLY | O_CREAT | O_TRUNC), m)
+//posix_spawn_file_actions_adddup2(&action, 1, 2)
+
+let args = ["/usr/bin/env", "bash", "-i", "-c", #"echo -n "$PS1""#]
+let c_args = args.map { $0.withCString(strdup)! }
+defer { for arg in c_args { free(arg) } }
+
+var pid = pid_t()
+let rv = posix_spawnp(&pid, c_args[0], &action, nil, c_args + [nil], environ)
+guard rv == 0 else {
+  // Should get errno
+  exit(1)
+}
+print(pid)
+
+var exitCode: Int32 = 0
+waitpid(pid, &exitCode, 0)
+
+//print("Process exited with code \(exitCode)")
+
+let rawPS1 = try! String(contentsOfFile: "/tmp/ps1")
+
+// MARK: Convert to coloring, replacements, etc.
+//let s = String(contentsOfFile: .init(fileURLWithPath: "/tmp/ps1"))
+let convertedPS1 = rawPS1.replacingOccurrences(of: #"\033"#, with: "\u{001B}").replacingOccurrences(of: #"\e"#, with: "\u{001B}") //escape chars
+                        .replacingOccurrences(of: #"\["#, with: "").replacingOccurrences(of: #"\]"#, with: "") //used to tell bash the're format chars and not printed
+                        .replacingOccurrences(of: #"\w"#, with: ProcessInfo.processInfo.environment["PWD"]!.replacingOccurrences(of: "/Users/\(ProcessInfo.processInfo.userName)", with: "~")) //current working directory
+                        .replacingOccurrences(of: #"\W"#, with: ProcessInfo.processInfo.environment["PWD"]!.split(separator: "/").last!) //CWD basename
+                        //.replacingOccurrences(of: #"\d"#, with: "df.dateFormat = "E MMM d"") //should be done every time we print prompt
+                        .replacingOccurrences(of: #"\h"#, with: ProcessInfo.processInfo.hostName)
+                        .replacingOccurrences(of: #"\H"#, with: ProcessInfo.processInfo.hostName)
+                        .replacingOccurrences(of: #"\j"#, with: "0") //jobs — since we won't allow backgrounding, always 0
+                        .replacingOccurrences(of: #"\l"#, with: "[repl]") //"The basename of the shell's terminal device name." We don't have one
+                        .replacingOccurrences(of: #"\s"#, with: CommandLine.arguments[0].split(separator: "/").last!) //"The name of the shell, the basename of $0 (the portion following the final slash)."
+                        //.replOcc("t", "T", "@") //time, must be done every time we print prompt
+                        //\t   The time, in 24-hour HH:MM:SS format. 
+                        //\T   The time, in 12-hour HH:MM:SS format. 
+                        //\@   The time, in 12-hour am/pm format.
+                        .replacingOccurrences(of: #"\u"#, with: ProcessInfo.processInfo.userName)
+                        .replacingOccurrences(of: #"\v"#, with: "1.0") //version major.minor, no patch
+                        .replacingOccurrences(of: #"\v"#, with: "1.0.0") //version major.minor.patch
+                        //.replacingOccurrences(of: #"\!"#, with: "1.0") //history number
+                        //.replacingOccurrences(of: #"\#"#, with: "1.0") //command number (i.e. length of commands list)
+                        //.replacingOccurrences(of: #"\$"#, with: "1.0") //'#' iff root else '$'
+print(convertedPS1)
+let cmdPrompt = convertedPS1 + "\u{001B}[1;32m\(command)\u{001B}[00m "
+
+// MARK: - Begin REPL
 print("Initializing REPL with command: \(command)")
 print("Use ^D to exit")
 print()
